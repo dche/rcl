c716471cc660de691baffef38b1040d52e4e525b
Diego Che <chekenan@gmail.com>
2010-08-02 05:20:06 +0800

    A rubyish interface of OpenCL.

--- lib/opencl.rb
+++ lib/opencl.rb
@@ -1,8 +1,184 @@
 # encoding: utf-8
 #
-# copyright (c) 2010, Diego Che (chekenan@gmail.com)
+# copyright (c) 2010, Che Kenan (chekenan@gmail.com)
 
-require File.join(File.dirname(__FILE__), 'opencl/capi')
+require File.join(File.dirname(__FILE__), 'opencl/capi_support')
 
+
+# A rubyish interface to OpenCL.
 module OpenCL
+  class ProgramBuildError < Capi::CLError; end
+
+  class CLError < Capi::CLError; end
+
+  # Compiled code that can be executated on OpenCL devices.
+  class Program
+    # Capi::Context
+    attr_reader :context
+    # Capi::CommandQueue
+    attr_reader :queue
+    # The source code
+    attr_reader :source
+
+    def initialize(src, compile_options = '')
+      begin
+        devs = Capi.devices(Capi::CL_DEVICE_TYPE_GPU, nil)
+        devs = Capi.devices(Capi::CL_DEVICE_TYPE_CPU, nil) if devs.empty?
+
+        @context = Capi::Context.new nil, devs
+        @device = devs.first
+        @queue = @context.create_command_queue @device
+        @kernels = {}
+      rescue Capi::CLError => e
+        raise CLError, e.message
+      end
+      self.compile src, compile_options
+    end
+    
+    # 
+    def compile(src, opts = '')
+      return self if src == @source
+      # TODO: protect this. Can't call #call when compile new source.
+      begin
+        @program = @context.create_program src
+        @program.build [@device], opts, nil
+      rescue Capi::CLError => e
+        if e.message =~ /\(#{Capi::CL_BUILD_PROGRAM_FAILURE}\)/ # FIXME: ugly coupling.
+          raise ProgramBuildError, @program.build_info(@device, Capi::CL_PROGRAM_BUILD_LOG)
+        else
+          raise CLError, e.message
+        end
+      end
+      @kernels.clear
+      @source = src
+      self
+    end
+    
+    # Executes a kernel.
+    #
+    # Kernel arguments is specified in a Hash in the form {value => type}
+    def call(kernel, dim, size, args)      
+      begin
+        k = @kernels[kernel] || @kernels[kernel] = @program.create_kernel(kernel.to_s)
+        
+        if args.size != k.info(Capi::CL_KERNEL_NUM_ARGS)
+          raise ArgumentError, "Wrong number of kernel arguments (#{args.size} for #{k.info(Capi::CL_KERNEL_NUM_ARGS)})."
+        end
+
+        args.each_with_index do |kv, idx|
+          value, type = kv
+          
+          case type
+          when :mem
+            k.set_arg idx, value.memory
+          else
+            k.set_arg_with_type idx, type, value
+          end
+        end
+        self.queue.enqueue_NDRange_kernel(k, dim, size, nil, nil)
+        self.queue.finish
+      rescue Capi::CLError => e
+        raise CLError, e.message
+      end
+      self
+    end
+    
+    def method_missing(meth, *args, &blk)
+      if @kernels.has_key?(meth)
+        self.call(meth, *args) 
+      else
+        begin
+          k = @program.create_kernel(meth.to_s)
+          @kernels[meth] = k
+          self.call(meth, *args)
+        rescue Capi::CLError => e
+          super
+        end
+      end
+    end
+    
+    def to_s
+      self.source
+    end
+  end
+  
+  # Buffer constains data on which a Program's kernels operate.
+  # A Buffer object represents an OpenCL Memory object.
+  #
+  # A Buffer is always attached with a Program object. A Buffer created with
+  # one Program can't be used in anohther program.
+  #
+  class Buffer
+    # Buffer size
+    attr_reader :size
+    # Capi::Memory
+    attr_reader :memory
+
+    # Creates a buffer within the same context as of +program+.
+    def initialize(program, size, io_flag = :in_out)
+      @program = program
+      @io = io_flag
+
+      cl_io = Capi::CL_MEM_READ_WRITE
+      case @io
+      when :in_out
+      when :in
+        cl_io = Capi::CL_MEM_READ_ONLY
+      when :out
+        cl_io = Capi::CL_MEM_WRITE_ONLY
+      else
+        raise ArgumentError, 'Invalid memory in/out flag.'
+      end
+
+      begin
+        @memory = program.context.create_buffer(cl_io, size, nil)
+        @size = size
+      rescue Capi::CLError => e
+        raise CLError, e.message
+      end
+    end
+    
+    def method_missing(meth, *args, &blk)
+      super
+    end
+    
+    # Read from the device memory.
+    def store_data_to(pointer)
+      if self.out?
+        begin
+          @program.queue.enqueue_read_buffer(self.memory, true, 
+                                             0, pointer.byte_size,
+                                             pointer, nil);
+          @program.queue.finish
+        rescue Capi::CLError => e
+          raise CLError, e.message
+        end
+      end
+      self
+    end
+    alias :read :store_data_to
+    
+    # Write to the device memroy.
+    def get_data_from(pointer)
+      if self.in?
+        begin
+          @program.queue.enqueue_write_buffer(self.memory, true,
+                                              0, pointer.byte_size,
+                                              pointer, nil);
+        rescue Capi::CLError => e
+          raise CLError, e.message
+        end
+      end
+      self
+    end
+    alias :write :get_data_from
+    
+    def in?
+      @io != :out
+    end
+    
+    def out?
+      @io != :in
+    end
+  end
 end
---
+++ spec/program_spec.rb
@@ -0,0 +1,149 @@
+
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+include OpenCL
+
+valid_src = <<-EOK
+  __kernel void
+  dot_product (__global const float4 *a,
+               __global const float4 *b,
+               __global float *c)
+  {
+    int gid = get_global_id(0);
+
+    c[gid] = dot(a[gid], b[gid]);
+  }
+EOK
+
+invalid_src = <<-EOK
+  __kernel void
+  dot_product (__global const float4 *a,
+               __global const float4 *b,
+               __global float *c)
+  {
+    int gid = get_global_id(0)
+
+    c[gid] = dot(a[gid], b[gid]);
+  }
+EOK
+
+multi_kernel_src = <<-EOK
+  __kernel void
+  dot_product (__global const float4 *a,
+               __global const float4 *b,
+               __global float *c)
+  {
+    int gid = get_global_id(0);
+
+    c[gid] = dot(a[gid], b[gid]);
+  }
+  __kernel void
+  mult (__global const float *in,
+          __global float *out,
+          float fac)
+  {
+    int gid = get_global_id(0);
+    out[gid] = in[gid] * fac;
+  }
+EOK
+
+describe Program do
+  the 'Program::new' do
+    prog = OpenCL::Program.new valid_src
+    prog.context.should.is_a Capi::Context
+    prog.queue.should.is_a Capi::CommandQueue
+  end
+  
+  it 'should reject invalid source' do
+    should.raise(ProgramBuildError) { OpenCL::Program.new invalid_src }
+  end
+  
+  it 'should reject invalid compile options' do
+    should.not.raise(Exception) { OpenCL::Program.new valid_src, '-cl-fast-relaxed-math' }
+    should.raise(CLError) { OpenCL::Program.new valid_src, '-cl-ruby' }
+  end
+  
+  it 'should be able to change program source' do
+    prog = OpenCL::Program.new valid_src
+    prog.source.should.equal valid_src
+    prog.compile multi_kernel_src
+    prog.source.should.equal multi_kernel_src
+  end
+  
+  the 'Buffer::new' do
+    prog = OpenCL::Program.new valid_src
+    buff = Buffer.new prog, 1024
+    buff.should.be.in
+    buff.should.be.out
+  end
+  
+  the '#call' do
+    prog = OpenCL::Program.new valid_src
+    ptr = HostPointer.new :cl_float4, 2
+    ptr[0] = [1, 2, 3, 4]
+    ptr[1] = [4, 3, 2, 1]
+    
+    m1 = Buffer.new prog, ptr.byte_size, :in
+    m1.write ptr
+    
+    ptr[0] = [5, 6, 7, 8]
+    ptr[1] = [8, 7, 6, 5]
+    m2 = Buffer.new prog, ptr.byte_size, :in
+    m2.write ptr
+    
+    pres = HostPointer.new :cl_float, 2
+    m3 = Buffer.new prog, pres.byte_size, :out
+    
+    prog.call(:dot_product, 1, [2], m1 => :mem, m2 => :mem, m3 => :mem)
+    m3.read pres
+    pres[0].should.equal 70
+    pres[0].should.equal pres[1]
+  end
+  
+  the 'multiple kernels should work' do
+    prog = nil
+    should.not.raise(Exception) { prog = OpenCL::Program.new multi_kernel_src }
+    
+    p1 = HostPointer.new :cl_float, 4
+    p1.assign [1, 2, 3, 4].pack('f4')
+    p1[2].should.equal 3
+    
+    m1 = Buffer.new prog, p1.byte_size, :in
+    m1.write p1
+    m2 = Buffer.new prog, p1.byte_size, :out
+    prog.call(:mult, 1, [4], m1 => :mem, m2 => :mem, 10 => :cl_float)
+    m2.read p1
+    p1[0].should.equal 10
+    p1[1].should.equal 20
+    p1[2].should.equal 30
+    p1[3].should.equal 40
+    
+    prog.call(:mult, 1, [4], m1 => :mem, m2 => :mem, 100 => :cl_float)
+    m2.read p1
+    p1[0].should.equal 100
+    p1[1].should.equal 200
+    p1[2].should.equal 300
+    p1[3].should.equal 400
+  end
+  
+  the 'method_missing' do
+    prog = OpenCL::Program.new multi_kernel_src
+    
+    p1 = HostPointer.new :cl_float, 4
+    p1.assign [1, 2, 3, 4]
+    p1[2].should.equal 3
+    
+    m1 = Buffer.new prog, p1.byte_size, :in
+    m1.write p1
+    m2 = Buffer.new prog, p1.byte_size, :out
+    should.raise(NoMethodError) { prog.add }
+    should.not.raise(NoMethodError) { 
+      prog.mult(1, [4], m1 => :mem, m2 => :mem, 10 => :cl_float)
+    }
+    m2.read p1
+    p1[0].should.equal 10
+    p1[1].should.equal 20
+    p1[2].should.equal 30
+    p1[3].should.equal 40
+  end
+end
