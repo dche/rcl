f917736004f9ca5711ff0723e79e2d0f25ee595f
Diego Che <chekenan@gmail.com>
2010-10-07 06:07:08 +0800

    Spec for String monkey patching.

---
+++ spec/string_spec.rb
@@ -0,0 +1,23 @@
+
+require_relative 'spec_helper'
+require_relative '../lib/opencl/string'
+
+describe String do
+  
+  it 'should complain if the module name is invalid.' do
+    should.raise(ArgumentError) {
+      "".rcl_include 'no_such_module'
+    }
+  end
+  
+  it 'should prepend contents of the included module to itself.' do
+    ''.rcl_include('random').should.not.be.empty
+  end
+
+end
+
+describe OpenCL do
+  the '#utility_modules' do
+    OpenCL.utility_modules.should.include('random')
+  end
+end
