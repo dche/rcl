a050a4da1502078514bad07e04bb208d6d1edfcd
Diego Che <chekenan@gmail.com>
2010-10-09 03:35:34 +0800

    Now Capi::CLError's message is the error code.

--- spec/cl_error_spec.rb
+++ spec/cl_error_spec.rb
@@ -3,16 +3,12 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 include OpenCL
 
 describe CLError do
-  it 'should reject invalid error message.' do
-    should.raise(ArgumentError) { CLError.new '(012) This is not a valid message.' }
-    should.raise(ArgumentError) { CLError.new '(12)  This is not a valid message.' }
-    should.raise(ArgumentError) { CLError.new '(a12) This is not a valid message.' }
-    should.not.raise(Exception) { CLError.new '(12) This is a valid message.' }
-
+  it 'should reject invalid error code.' do
+    should.raise(ArgumentError) { CLError.new '0' }
+    should.raise(ArgumentError) { CLError.new 'a12' }
+CLError.new "#{Capi::CL_BUILD_PROGRAM_FAILURE}"
+    should.not.raise(Exception) { CLError.new "#{Capi::CL_BUILD_PROGRAM_FAILURE}" }
+    should.not.raise(Exception) { CLError.new '100_0000' }
   end
 
-  it 'should strip the error code from error message.' do
-    err = CLError.new '(12) This is a valid message.'
-    err.message.should.equal 'This is a valid message.'
-  end
 end
