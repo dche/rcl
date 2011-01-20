493c396533ba390a4983db32c490566f56523fc6
Diego Che <chekenan@gmail.com>
2011-01-21 00:34:48 +0800

    perftools is cool. A wrapper to profile often.

--- bm/bm_helper.rb
+++ bm/bm_helper.rb
@@ -5,3 +5,20 @@ require File.join(File.dirname(__FILE__), '../lib/opencl')
 
 include Benchmark
 include OpenCL
+
+def profile(name, times, &blk)
+  begin
+    require 'perftools'
+
+    output = "/tmp/#{name}_profile"
+    svg_output = "~/Desktop/#{name}_profile.svg"
+
+    PerfTools::CpuProfiler.start(output) do
+      times.to_i.times &blk
+    end
+    `pprof.rb --svg #{output} >> #{svg_output}`
+    puts "Check the graphical profiling result in #{svg_output}"
+  rescue LoadError
+    warn 'The excellent gem "perftools.rb" is not installed.'
+  end
+end
--- bm/reduce_profile.rb
+++ bm/reduce_profile.rb
@@ -3,17 +3,8 @@
 require File.join(File.dirname(__FILE__), 'bm_helper')
 require File.join(File.dirname(__FILE__), '../lib/opencl/vector')
 
-begin
-require 'perftools'
-
 vec = Vector.rand 1_000_000
 
-PerfTools::CpuProfiler.start("/tmp/reduce_profile") do
-  1000.times do
-    vec.sum
-  end
-end
-
-rescue LoadError
-  puts 'perftools.rb is not installed.'
+profile(:reduce, 1_000) do
+  vec.sum
 end
