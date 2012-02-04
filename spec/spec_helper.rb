e02c76ef52ce0ef21d05be4957f325a62b423bf8
Che Kenan <dche@Kenan-Ches-MacBook.local>
2012-02-05 03:09:41 +0800

    Use SimpleCov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,6 @@
 # encoding: utf-8
 
 require 'rubygems'
-
-def coveragable?
-  RUBY_ENGINE == 'ruby' && RUBY_VERSION >= '1.9'
-end
-
-if coveragable?
-  require 'coverage'
-
-  at_exit do
-    Coverage.result
-  end
-end
-
 require 'bacon'
 
 module Bacon
@@ -27,7 +14,18 @@ module Bacon
 end
 Bacon.extend Bacon::TapOutput
 
-Coverage.start if coveragable?
+begin
+  require 'simplecov'
+
+  SimpleCov.start do
+    project_name "rcl"
+    coverage_dir "cov"
+    root "#{pwd}/.."
+    use_merging true
+  end
+rescue LoadError
+end
+
 
 require File.join(File.dirname(__FILE__), '../lib/opencl')
 include OpenCL
