565684fa860868d6c52d6d48165047f109440a56
Che Kenan <chekenan@gmail.com>
2012-06-24 18:29:10 +0800

    Should re-write this part (NDArray, Vector, Matrix etc.) in a separate gem that depends on opencl.

--- lib/opencl/ndarray.rb
+++
@@ -1,7 +0,0 @@
-
-require File.join(File.dirname(__FILE__), 'library/random')
-
-require File.join(File.dirname(__FILE__), 'ndarray/ndarray')
-require File.join(File.dirname(__FILE__), 'ndarray/gen')
-require File.join(File.dirname(__FILE__), 'ndarray/integer')
-require File.join(File.dirname(__FILE__), 'ndarray/math')
