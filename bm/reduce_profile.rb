4c2c89eb2f6a723767be9052a42baa870565d55c
Che Kenan <chekenan@gmail.com>
2012-06-26 00:59:16 +0800

    Remove fft.

--- bm/reduce_profile.rb
+++
@@ -1,9 +0,0 @@
-
-require File.join(File.dirname(__FILE__), 'bm_helper')
-require File.join(File.dirname(__FILE__), '../lib/opencl/vector')
-
-vec = Vector.ones 1_000_000
-
-profile(:reduce, 1_000) do
-  vec.sum
-end
